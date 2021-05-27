445d119c1e59d9c1a238e7415bb72287494f823d
nishit-popat <nisitpopat9999@gmail.com>
2021-05-27 21:05:11 +0530

    AlterPositiveAndNegative Updated

--- Array/AlterPositiveNegative.java
+++ Array/AlterPositiveNegative.java
@@ -24,6 +24,8 @@ Arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
 Output:
 5 -5 2 -2 4 -8 7 1 8 0 
 
+
+This can be solved in O(1) extra space as well that is shown by another function, look at both functions.
 */
 
 public class AlterPositiveNegative {
@@ -34,8 +36,11 @@ public class AlterPositiveNegative {
         int n1 = arr1.length;
         int n2 = arr2.length;
 
-        alternateArray(arr1,n1);
-        alternateArray(arr2, n2);
+        //alternateArray(arr1,n1);
+        //alternateArray(arr2, n2);
+
+        alternateArrayInBigO1(arr1, n1);
+        alternateArrayInBigO1(arr2, n2);
         
     }
 
@@ -78,6 +83,36 @@ public class AlterPositiveNegative {
             arr[c]=pos.get(j);
             j++;
             c++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void alternateArrayInBigO1(int arr[], int n){
+        //first we will arrange all negative numbers at left side of the array
+		//if we arrange the negative numbers at left side then positive numbers will 
+        //autometically arrange at right side
+
+        int start = 0; //for negative numbers
+        for(int i=0;i<n;i++){
+            if(arr[i] < 0){
+                //swap arr[i] and arr[start]
+                int temp = arr[i];
+                arr[i] = arr[start];
+                arr[start] = temp;
+                start++;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(start);
+
+        //at start variable positive index starts
+        for(int i=1;i<n;i+=2){
+            //swap arr[start] and arr[i]
+            int temp = arr[i];
+            arr[i] = arr[start];
+            arr[start] = temp;
+            start++;
         }
         System.out.println(Arrays.toString(arr));
     }
